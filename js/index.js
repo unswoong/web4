f943a65307c524fd44bd67deaf0a675e544b2fe3
sunwoong <art6456@naver.com>
2023-03-17 12:40:40 +0900

    web4
    modify

--- js/index.js
+++ js/index.js
@@ -2,18 +2,28 @@ const header = document.querySelector("header");
 let 이전좌표 = window.scrollY;
 
 window.addEventListener("scroll", () => {
+  hambergerButton.classList.add("active");
+  hambergerModal.classList.add("hide-modal");
+  hambergerModal.classList.add("go-back");
+
   if (window.scrollY < 100) {
     header.classList.remove("scrolled");
   } else {
     header.classList.add("scrolled");
   }
+  if (이전좌표 < window.scrollY) {
+    header.classList.add("hide-header");
+  } else {
+    header.classList.remove("hide-header");
+  }
+  이전좌표 = window.scrollY;
 });
 
 const hambergerButton = document.getElementById("hamberger");
 const hambergerModal = document.querySelector(".hamberger-modal");
+
 hambergerButton.addEventListener("click", (e) => {
   e.preventDefault();
-
   hambergerButton.classList.toggle("active");
   hambergerModal.classList.toggle("hide-modal");
   hambergerModal.classList.toggle("go-back");
